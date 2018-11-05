386519379cd1506f4b1cf9f8a19ea3d15291c1c3
Keiichii <Keiichii@users.noreply.github.com>
2018-11-05 22:42:38 +0300

    L3

---
+++ Unit 1/L2E1 2 bags all combos.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  2 20:57:59 2018
+
+@author: ART
+"""
+
+def yieldAllCombos(items):
+    """
+        Generates all combinations of N items into two bags, whereby each 
+        item is in one or zero bags.
+
+        Yields a tuple, (bag1, bag2), where each bag is represented as a list 
+        of which item(s) are in each bag.
+    """
+    N = len(items)
+    # enumerate the 2**N possible combinations
+    for i in range(3**N):
+        combo1 = []
+        combo2 = []
+        for j in range(N):
+            # test bit jth of integer i
+            #print(i, '>>', j, '=', i>>j, ':', '%3==', (i>>j)%2)
+            if (i //3**j) % 3 == 1:
+                combo1.append(items[j])
+            if (i //3**j ) % 3 == 2:
+                combo2.append(items[j])
+        yield (combo1, combo2)
+        
+z = yieldAllCombos([1,2,3])
+for _ in z:
+    print(_)
